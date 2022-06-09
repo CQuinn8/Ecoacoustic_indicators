8643daf454a9f7fd1f45a8a47cdb06ea0d6d0aa8
CQuinn8 <cq73@nau.edu>
2022-06-08 17:48:35 -0700

    updated pairedARU and GAMs

--- acoustic_indices/2_soundscape_modeling/0_by_site_GAM.R
+++ acoustic_indices/2_soundscape_modeling/0_by_site_GAM.R
@@ -11,6 +11,14 @@
 # 6) some GAM implementation: https://m-clark.github.io/generalized-additive-models/application.html
 # 7) posterior estimate with GAM: https://stats.stackexchange.com/questions/190348/can-i-use-bootstrapping-to-estimate-the-uncertainty-in-a-maximum-value-of-a-gam
 # 8) https://kevintshoemaker.github.io/NRES-746/Generalized%20Additive%20Models%20(GAMs).pdf
+# 9) Random effects post: https://stats.stackexchange.com/questions/552880/by-group-random-effect-gam
+
+# MODELING NOTES
+# NDSI: (June8) complete
+# ACI: (June8) need to check in on random effects otherwise good
+# ADI: (June8) Still needs help, not a great transformation - non constant residul var, nonlinear observed vs fitter, etc.
+
+
 
 
 
@@ -24,15 +32,20 @@ library(mgcv)
 library(tidyverse)
 library(outliers)
 library(gratia)
-
+source('//shares.hpc.nau.edu/cirrus/projects/tropics/users/cquinn/s2l/code/paper1-AcousticIndices/utility_fxs.R')
 wd = '//shares.hpc.nau.edu/cirrus/projects/tropics/users/cquinn/s2l/paper1-AcousticIndices/results/'
-# abgqi_df = fread(paste0(wd, 'ABGQI_inference/averages/site_avg_ABGQI.csv')) %>%
-#   select(-contains('var')) %>%
-#   select(site, wavs, contains('mean'))
-# corrected ABGQI data (May 31 2022)
-abgqi_df = fread(paste0(wd, 'paired_ARUs/corrected_data/corrected_site_abgqi_31May2022.csv'))
+abgqi_df = fread(paste0(wd, 'ABGQI_inference/averages/site_avg_ABGQI.csv')) %>%
+  select(-contains('var')) %>%
+  select(site, wavs, contains('mean'), -Unidentified_mean) %>%
+  mutate(ARU = substr(site, 4,5)) %>%
+  rename(Anthropophony = Anthropophony_mean, Biophony = Biophony_mean, Geophony = Geophony_mean,
+         Quiet = Quiet_mean, Interference = Interference_mean)
 indices_df = fread(paste0(wd, 'acoustic_indices_aggregation/averages/site_avg_acoustic_indices.csv'))
-df = merge(indices_df, abgqi_df, by = 'site')
+
+# corrected ABGQI data (May 31 2022)
+# abgqi_df = fread(paste0(wd, 'paired_ARUs/corrected_data/corrected_site_abgqi_03June2022.csv'))
+# indices_df = fread(paste0(wd, 'paired_ARUs/corrected_data/corrected_site_acousticindices_03June2022.csv'))
+# df = merge(indices_df, abgqi_df, by = 'site')
 
 # responses
 responses = c("ACI", "ADI", "AEI", "NDSI", 'NDSI_A', 'NDSI_B', "BI", 
@@ -40,68 +53,69 @@ responses = c("ACI", "ADI", "AEI", "NDSI", 'NDSI_A', 'NDSI_B', "BI",
               "zcr_max", "zcr_mean", "zcr_min")
 
 predictors = c('Anthropophony', 'Biophony', 'Geophony', 
-               'Quiet', 'Interference', 'Unidentified')
+               'Quiet', 'Interference')
 
-response_exam = function(df, response){
-  gathered_df = df %>%
-    gather(variable, value, -y)
-  
-  # y ~ x
-  ggplot(gathered_df, aes(x = value, y = y)) +
-    geom_point(alpha = 0.2) +
-    facet_wrap(~variable, scales = 'free') +
-    stat_smooth(method = 'gam', se = FALSE) +
-    labs(title = paste0(response, 'with GAM curve', x = "Predictor value"))
-}
-toLogit<-function(x){
-  cx<-ifelse(x==0,0.00001,
-             ifelse(x==1,0.99999,x))
-  lgx<-log(cx)-log(1-cx)
-  return(lgx)
-}
+# Total minutes pre-analyses
+sum(abgqi_df$wavs) # 741061
 
+# Clean sites with anamolous values
+# 1. Top 0.1% of Sites with significant interference (e.g., Int > 4sd(Int))
+# 2. Sites with any value == 1
+# 3. Zero averages -> median value
+# Interference
+int_sd = sd(abgqi_df$Interference)
+mod_df = abgqi_df %>%
+  filter(Interference <= 4 * int_sd) # 20 sites
 
-# Drop sites with any 0 values
-mod_df = abgqi_df
+# only one site with any value == 1 (quiet)
+mod_df = mod_df %>%
+  filter(Quiet < 1)
+
+# account for zero values
 mod_df[mod_df == 0] = NA
-mod_df = mod_df[complete.cases(mod_df),] # n = -27
-mod_df$ARU = substr(mod_df$site, 4,5)
 
-# # Based on Wilcoxon tests and corrections we will remove LGs
-# mod_df = mod_df %>%
-#   filter(ARU == 'am')
-# 
-# # results in dropping:
-# 1247 - 994 # 253 sites were LG
-# sum(abgqi_df$wavs) - sum(mod_df$wavs) # 155,847 recordings
-# 
-# # AM has
-# nrow(mod_df) # 994 sites
-# sum(mod_df$wavs) #585,214 recordings
+# Which rows contain zeroes
+zeros_df = mod_df[!complete.cases(mod_df),] # n = 24: 22 are quiet, 1 Anthro & Geo, 1 Int
 
+# calculate medians
+medians = mod_df %>%
+  summarise(across(where(is.numeric), ~ median(.x, na.rm = TRUE)))
 
+# input medians for 0 (now NA) values
+mod_df = mod_df %>% 
+  mutate(across(where(is.numeric), .fns = ~ifelse(is.na(.x), median(.x, na.rm=TRUE), .x)))
+
+# Number of minutes in model data
+sum(mod_df$wavs) #731634
 
 ###########################################
-# NDSI
-i = responses[4]
+################# NDSI ####################
+i = "NDSI"
 temp_y = data.frame(site = indices_df$site, NDSI = indices_df[[i]])
 temp_df = merge(x = temp_y, y = mod_df) %>% 
-   select(-c(site, ARU))
+  mutate(ARU = factor(ARU)) %>%
+   select(-c(site))
+
+# visualize data
+temp_df %>%
+  gather(soundType, value, -NDSI, -ARU) %>%
+  ggplot(aes(x = value, y = NDSI, colour = ARU)) +
+    geom_point(alpha = 0.3) +
+    geom_smooth(method = "gam") +
+    facet_wrap(~soundType)
+  
+# add transformed NDSI
+temp_df$posNDSI = temp_df$NDSI + 1
+temp_df$beta_NDSI = (temp_df$NDSI + 1)/2
+temp_df$sq_beta_NDSI = (temp_df$beta_NDSI)^2
 
 # visualize pairs of corr
 temp_df %>%
   GGally::ggpairs(aes(alpha = 0.05), progress = FALSE)
 
-# add transformed NDSI
-# temp_df$logit_NDSI = toLogit((temp_df$NDSI + 1)/2)
-temp_df$posNDSI = temp_df$NDSI + 1
-temp_df$beta_NDSI = (temp_df$NDSI + 1)/2
-temp_df$sq_beta_NDSI = (temp_df$beta_NDSI)^2
-
- 
 ###### TESTING using: https://www.youtube.com/watch?v=Ukfvd8akfco
 mod1 = gam(NDSI ~ 
-              Anthropophony+ 
+              Anthropophony + 
               Biophony + 
               Geophony + 
               Quiet + 
@@ -109,6 +123,7 @@ mod1 = gam(NDSI ~
             data = temp_df)
 summary(mod1)
 
+# model using beta transformed NDSI
 mod2 = gam(beta_NDSI ~ 
             s(Anthropophony) + 
             s(Biophony) + 
@@ -122,125 +137,280 @@ summary(mod2) # model summary
 
 AIC(mod1, mod2)
 
-
+# add ARU random intercept effect
 mod3 = gam(beta_NDSI ~ 
              s(Anthropophony) + 
              s(Biophony) + 
+             s(Geophony) + 
              s(Quiet) + 
-             s(Interference),
+             s(Interference) +
+             s(ARU, bs = 're'), # random intercept
            data = temp_df,
            family = betar(),
            method = 'REML')
-summary(mod3)
-
+summary(mod3) # model summary
 AIC(mod2, mod3)
+variance_comp(mod3)
 
-draw(mod3) # nice visualization tool from gratia for partial effects
 
-k.check(mod3) 
+# Add random slopes
+mod4 = gam(beta_NDSI ~ 
+             ARU + # must include base factor to interpret offset of "by"
+             s(Anthropophony, by = ARU, bs = 're') + 
+             s(Biophony, by = ARU, bs = 're') + 
+             s(Geophony, by = ARU, bs = 're') + 
+             s(Quiet, by = ARU, bs = 're') + 
+             s(Interference, by = ARU, bs = 're') +
+             s(ARU, bs = 're'),
+           data = temp_df,
+           family = betar(),
+           method = 'REML')
+summary(mod4)
+
+AIC(mod3, mod4)
+
+# Model 3 appears more parsimoniuous
+# remove geophony
+mod5 = gam(beta_NDSI ~ 
+             s(Anthropophony) + 
+             s(Biophony) + 
+             s(Quiet) + 
+             s(Interference) +
+             s(ARU, bs = 're'), # random intercept
+           data = temp_df,
+           family = betar(),
+           method = 'REML')
+summary(mod5)
+AIC(mod3, mod5)
+
+draw(mod5) # nice visualization tool from gratia for partial effects
+
+k.check(mod5) 
 
 #appraise(mod2, method = "simulate") 
 # QQ should follow 1:1 wihtin the reference band (do data follow assumptions of model well) some do not
 # residuals should be approx gaussian 
 par(mfrow = c(2, 2))
-vis.gam(mod3, theta = 65)
+vis.gam(mod5, theta = 65)
 
 #appraise(mod8, method = "simulate") # only works with gaussian data
-gam.check(mod3, rep = 500) # initial Anthro k is too small there is more unexplained information not captured
-                           # large k doesn't allow for more modeling freedom
-                           # for rep = 500 does the QQ roughly fall within confidence interval? Not at the upper tail
-                           # histogram is roughly gaussian, Resp vs fitted is roughly 1:1, resids vs linear pred is potentially problematic
+gam.check(mod5, rep = 500) # for rep = 500 does the QQ roughly fall within confidence interval? Not at the upper tail
+                           # histogram is roughly gaussian, Resp vs fitted is roughly 1:1, resids vs linear pred may have decreasing variance
 
-variance_comp(mod8)
+
+
+###########################################
+# ACI
+i = "ACI"
+temp_y = data.frame(site = indices_df$site, ACI = indices_df[[i]])
+temp_df = merge(x = temp_y, y = mod_df) %>% 
+  mutate(ARU = factor(ARU)) %>%
+  select(-c(site,wavs))
+
+# visualize data
+temp_df %>%
+  gather(soundType, value, -ACI, -ARU) %>%
+  ggplot(aes(x = value, y = ACI, colour = ARU)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = "gam") +
+  facet_wrap(~soundType)
+ggplot(temp_df, aes(x = ACI, fill = ARU)) +
+  geom_histogram()
+
+
+# add transformed 
+# bring ACI down to 0
+temp_df$ACI = temp_df$ACI - min(temp_df$ACI)
+temp_df$log_ACI = log(temp_df$ACI + 1e-7)
+temp_df$sqrt_ACI = sqrt(temp_df$ACI)
+
+# remove one extremely high value
+outlier(temp_df$ACI)
+temp_df = temp_df %>%
+  filter(ACI < outlier(ACI))
+# faraway::halfnorm(temp_df$ACI)
+
+# visualize pairs of corr
+temp_df %>%
+  GGally::ggpairs(aes(alpha = 0.05))
+
+
+# fit a model
+mod1 = gam(ACI ~ 
+                 s(Anthropophony) + 
+                   s(Biophony) + 
+                   s(Geophony) + 
+                   s(Quiet) + 
+                   s(Interference),
+               data = temp_df,
+               family = gaussian,
+               method = 'REML')
+summary(mod1)
+
+# add random int
+mod2 = gam(ACI ~ 
+             s(Anthropophony) + 
+             s(Biophony) + 
+             s(Geophony) + 
+             s(Quiet) + 
+             s(Interference) +
+             s(ARU, bs = 're'),
+           data = temp_df,
+           family = gaussian,
+           method = 'REML')
+summary(mod2)
+
+AIC(mod1, mod2)
+mod = mod2
+draw(mod) 
+appraise(mod, method = "simulate") # Extreme upper QQ plot behavior, increasing variance in Resids plot
+
+# still strange behavior in QQ plot - try sqrtAIC
+mod3 = gam(sqrt_ACI ~ 
+             s(Anthropophony) + 
+             s(Biophony) + 
+             s(Geophony) + 
+             s(Quiet) + 
+             s(Interference) +
+             s(ARU, bs = 're'),
+           data = temp_df,
+           family = gaussian,
+           method = 'REML')
+summary(mod3)
+
+AIC(mod2, mod3)
+
+mod = mod3
+draw(mod) # nice visualization tool from gratia for partial effects
+k.check(mod) 
+appraise(mod, method = "simulate") # only works with gaussian data
+gam.check(mod, rep = 500)
+# appears to have pulled the upper QQ plot variation down and more normal Residuals plot
+
+# increase geophony basis functions per gam.check
+mod4 = gam(sqrt_ACI ~ 
+             s(Anthropophony) + 
+             s(Biophony) + 
+             s(Geophony, k = 25) + 
+             s(Quiet) + 
+             s(Interference) +
+             s(ARU, bs = 're'),
+           data = temp_df,
+           family = gaussian,
+           method = 'REML')
+summary(mod4)
+
+AIC(mod3, mod4)
+
+mod = mod4
+draw(mod) # nice visualization tool from gratia for partial effects
+k.check(mod) 
+appraise(mod, method = "simulate") # only works with gaussian data
+gam.check(mod, rep = 500)
+
+
+###########################################
+################# ADI #####################
+i = "ADI"
+temp_y = data.frame(site = indices_df$site, ADI = indices_df[[i]])
+temp_df = merge(x = temp_y, y = mod_df) %>% 
+  mutate(ARU = factor(ARU)) %>%
+  select(-c(site,wavs))
+
+# visualize data
+temp_df %>%
+  gather(soundType, value, -ADI, -ARU) %>%
+  ggplot(aes(x = value, y = ADI, colour = ARU)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = "gam") +
+  facet_wrap(~soundType)
+ggplot(temp_df, aes(x = ADI, fill = ARU)) +
+  geom_histogram()
+
+
+# add transformed 
+temp_df$ADI2 = temp_df$ADI^2
+temp_df$ADI3 = temp_df$ADI^3
+
+# remove one extremely low value
+outlier(temp_df$ADI)
+temp_df = temp_df %>%
+  filter(ADI > outlier(ADI))
+
+# visualize pairs of corr
+temp_df %>%
+  GGally::ggpairs(aes(alpha = 0.05))
+
+
+# fit a model
+mod1 = gam(ADI ~ 
+             s(Anthropophony) + 
+             s(Biophony) + 
+             s(Geophony) + 
+             s(Quiet) + 
+             s(Interference),
+           data = temp_df,
+           family = gaussian,
+           method = 'REML')
+summary(mod1)
+
+mod2 = gam(ADI^3 ~ 
+             s(Anthropophony) + 
+             s(Biophony) + 
+             s(Geophony) + 
+             s(Quiet) + 
+             s(Interference),
+           data = temp_df,
+           family = gaussian,
+           method = 'REML')
+summary(mod2)
+AIC(mod1, mod2)
+
+mod = mod2
+draw(mod) 
+k.check(mod) 
+appraise(mod, method = "simulate") # only works with gaussian data
+gam.check(mod, rep = 500)
+
+mod3 = gam(ADI ~ 
+             s(Anthropophony) + 
+             s(Biophony) + 
+             s(Geophony) + 
+             s(Quiet) + 
+             s(Interference) +
+             s(ARU, bs = 're'),
+           data = temp_df,
+           family = gaussian,
+           method = 'REML')
+summary(mod3)
+AIC(mod1, mod3)
+
+mod = mod3
+draw(mod) 
+k.check(mod) 
+appraise(mod, method = "simulate") # only works with gaussian data
+gam.check(mod, rep = 500)
+
 
 
 # PREDICTIONS
 library(itsadug)
-pred = get_predictions(mod3, 
-                       cond = list(Anthropophony = seq(min(temp_df$Anthropophony, na.rm = TRUE),
-                                                            max(temp_df$Anthropophony, na.rm = TRUE),
-                                                            length.out = nrow(temp_df)), se = TRUE))
+# pred = get_predictions(mod4, 
+#                        cond = list(Anthropophony = seq(min(temp_df$Anthropophony, na.rm = TRUE),
+#                                                             max(temp_df$Anthropophony, na.rm = TRUE),
+#                                                             length.out = nrow(temp_df)), se = TRUE))
+n = nrow(temp_df)
+pred = get_predictions(mod4, 
+                       cond = list(Anthropophony = seq(-1, 1, length.out = n), se = TRUE))
 
 pred = pred %>%
   mutate(NDSI = 1)
 
 temp_df %>%
   ggplot(aes(x = Anthropophony, y = NDSI)) +
-    geom_point(alpha = 0.3) +
+  geom_point(alpha = 0.3) +
   geom_ribbon(data = pred, alpha = 0.4, aes(ymin = fit - CI, ymax = fit + CI), show.legend = F, fill = 'forestgreen') +
   geom_line(data = pred, aes(y = fit), show.legend = FALSE, color = 'forestgreen')
-
-
-
-
-###########################################
-# ACI
-i = responses[1]
-temp_y = data.frame(site = indices_df$site, ACI = indices_df[[i]])
-temp_df = merge(x = temp_y, y = mod_df) %>% 
-  select(-c(site,wavs))
-
-# visualize pairs of corr
-temp_df %>%
-  GGally::ggpairs(aes(alpha = 0.05))
-
-# add transformed 
-temp_df$log_ACI = log(temp_df$ACI)
-temp_df$sqrt_ACI = sqrt(temp_df$ACI)
-outlier(temp_df$ACI)
-temp_df = temp_df %>%
-  filter(ACI < outlier(ACI))
-
-faraway::halfnorm(temp_df$ACI)
-
-# fit a model
-gaussian_fit = gam(ACI ~ 
-                 s(Anthropophony_mean)+ s(Biophony_mean) + s(Geophony_mean) + s(Quiet_mean) + s(Interference_mean),
-               data = temp_df,
-               family = gaussian(),
-               method = 'REML')
-
-# gaussian_fit2 = gam(ACI ~ s(Anthropophony_mean)+ s(Biophony_mean) + s(Geophony_mean) + s(Quiet_mean) + s(Interference_mean) +
-#                       te(Anthropophony_mean, Biophony_mean) + te(Anthropophony_mean, Quiet_mean) +
-#                       te(Biophony_mean, Quiet_mean) + te(Geophony_mean, Interference_mean),
-#                     data = temp_df,
-#                     family = gaussian(),
-#                     method = 'REML')
-
-lmfit = lm(ACI ~ Anthropophony_mean + Biophony_mean + Geophony_mean + Quiet_mean + Interference_mean,
-                            data = temp_df)
-
-# DIAGNOSTICS
-mod = gaussian_fit
-
-#fit # dof = dof of each covariate + 1. A dof ~ 1 is approximately linear
-plot(mod, residuals = TRUE, pch = 19, cex = 0.5, pages = 1, shade = T, scale = F)
-summary(mod)
-AIC(mod)
-par(mfrow = c(2, 2))
-gam.check(mod)
-vis.gam(mod, theta = 65)
-anova(mod)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
---
+++ utility_fxs.R
@@ -0,0 +1,147 @@
+
+################## PAIRED ARU FUNCTIONS #######################
+# read in paired ABGQI data
+# read in all pairs and create dataframes of each paired set of predictions by overlapping time
+abgqi_pair_reader = function(paired_df){
+  pair_list = list()
+  for(pair in seq_along(paired_df$SiteID)){
+    temp_row = paired_df[pair,]
+    print(paste0("Pair: ",pair))
+    
+    #read in csvs
+    am = read.csv(paste0(wd, 'results/paired_ARUs/paired_ABGQI/', temp_row$SiteID, '.csv')) %>%
+      dplyr::select(Anthro, Bio, Geo, Other, Quiet, wav, mfcc) %>%
+      dplyr::rename(Anthrophony = Anthro, Biophony = Bio, Geophony = Geo, Interference = Other, Quiet = Quiet, wavAM = wav) %>%
+      gather(variable, value, -wavAM, -mfcc)
+    lg = read.csv(paste0(wd, 'results/paired_ARUs/paired_ABGQI/', temp_row$Paired_site, '.csv')) %>%
+      dplyr::select(Anthro, Bio, Geo, Other, Quiet, wav, mfcc) %>%
+      dplyr::rename(Anthrophony = Anthro, Biophony = Bio, Geophony = Geo, Interference = Other, Quiet = Quiet, wavLG = wav) %>%
+      gather(variable, value, -wavLG, -mfcc)
+    
+    # build dateframe
+    am$site_AM = temp_row$SiteID
+    am$DD = substr(am$wav, 25, 26)
+    am$HH = substr(am$wav, 28, 29)
+    am$mm = substr(am$wav, 31, 32)
+    am$DDHHmm = paste0(am$DD, am$HH, am$mm)
+    
+    lg$site_LG = temp_row$Paired_site
+    lg$DD = substr(lg$wav, 25, 26)
+    lg$HH = substr(lg$wav, 28, 29)
+    lg$mm = substr(lg$wav, 31, 32)
+    lg$DDHHmm = paste0(lg$DD, lg$HH, lg$mm)
+    
+    pair_list[[pair]] = am %>%
+      inner_join(lg, by = c('DD','HH','mm','variable','mfcc')) %>%
+      dplyr::select(wavAM, mfcc, variable, value.x, value.y, DDHHmm.x, site_AM, site_LG) %>%
+      dplyr::rename(wav = wavAM, melspec = mfcc, soundType = variable, DDHHmm = DDHHmm.x, AM = value.x, LG = value.y) %>%
+      mutate(SiteID = pair)
+    
+  }
+  pair_preds = do.call(rbind, pair_list)
+}
+
+# read in acoustic index data
+  acoustic_index_pair_reader = function(paired_df){
+  pair_list = list()
+  for(pair in seq_along(paired_df$SiteID)){
+    temp_row = paired_df[pair,]
+    print(paste0("Pair: ",pair))
+    
+    #read in csvs
+    am = read.csv(paste0(wd, 'results/paired_ARUs/paired_acoustic_indices/', temp_row$SiteID, '.csv')) %>%
+      select(-YYYY, -MM, -DD, -hh, -mm, -DDhh, -hhmm) %>%
+      rename(site_AM = site, wav = file) %>%
+      gather(variable, value, -wav, -site_AM)
+    lg = read.csv(paste0(wd, 'results/paired_ARUs/paired_acoustic_indices/', temp_row$Paired_site, '.csv')) %>%
+      select(-YYYY, -MM, -DD, -hh, -mm, -DDhh, -hhmm) %>%
+      rename(site_LG = site, wav = file) %>%
+      gather(variable, value, -wav, -site_LG)
+    
+    # build dateframe
+    am$DD = substr(am$wav, 25, 26)
+    am$HH = substr(am$wav, 28, 29)
+    am$mm = substr(am$wav, 31, 32)
+    am$DDHHmm = paste0(am$DD, am$HH, am$mm)
+    
+    lg$DD = substr(lg$wav, 25, 26)
+    lg$HH = substr(lg$wav, 28, 29)
+    lg$mm = substr(lg$wav, 31, 32)
+    lg$DDHHmm = paste0(lg$DD, lg$HH, lg$mm)
+    
+    pair_list[[pair]] = am %>%
+      inner_join(lg, by = c('DD','HH','mm','variable')) %>%
+      dplyr::select(wav.x, variable, value.x, value.y, DDHHmm.x, site_AM, site_LG) %>%
+      dplyr::rename(wav = wav.x, acIndex = variable, DDHHmm = DDHHmm.x, AM = value.x, LG = value.y) %>%
+      mutate(SiteID = pair)
+    
+  }
+  pair_indices = do.call(rbind, pair_list)
+}
+
+
+# boxplots for ARU comparison
+index_boxplot = function(df){
+  df %>%
+    ungroup() %>%
+    select(AM, LG) %>%
+    gather(ARU, value) %>%
+    ggplot(aes(x = ARU, y = value)) + 
+    geom_jitter(width = 0.1, alpha = 0.1) + 
+    geom_boxplot(alpha = 0.4, outlier.shape = NA, notch = TRUE)
+}
+
+# corr plots for ARU comparison
+corr_plotter = function(df){
+  df%>%
+    ungroup() %>%
+    select(where(is.numeric)) %>%
+    select(contains(c("AM","LG"))) %>%
+    GGally::ggpairs(aes(alpha = 0.05), progress = FALSE)
+}
+
+# function to normalize data from 0 to 1
+min_max_norm <- function(x, min_x, max_x) {
+  (x - min_x) / (max_x - min_x)
+}
+
+center_scale = function(x , mu, sd){
+  (x - mu) / sd
+}
+
+
+# Mean independent (ie robust to outlier) estimate of scale for an array of values
+MAD_z = function(array){
+  med = median(array)
+  n = length(array)
+  
+  # derive median of all absolute deviations from the median
+  MAD_med = median(sapply(array, function(x) abs(x - med)))
+  
+  # apply MAD correction for unbiased normal distribution
+  MAD = 1.483 * MAD_med
+  
+  # median and MAD estimator of robust scores
+  est = sapply(array, function(x) (x - med)/MAD)
+}
+
+# IDentify outlier values using the MAD approach and threshold score at 99 percentile
+outlier_id = function(array){
+  MAD_values = MAD_z(array)
+  threshold = quantile(abs(MAD_values), probs = 0.99)
+  outlier_positions = which(abs(MAD_values) >= threshold)
+  return(outlier_positions)
+}
+
+toLogit <- function(x){
+  cx<-ifelse(x==0,0.00001,
+             ifelse(x==1,0.99999,x))
+  lgx<-log(cx /(1-cx))
+  return(lgx)
+}
+
+# x is the value to logit-transform from
+fromLogit<-function(x){
+  bt<-exp(x)/(1+exp(x))
+  return(bt)
+}
